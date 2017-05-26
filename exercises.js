5ce3b8b786a8709ccd866a34db332ddbef2a9519
mixelpixel <mixelpix@mac.com>
2017-05-25 23:10:59 -0400

    Homework 3

--- exercises.js
+++ exercises.js
@@ -7,18 +7,31 @@ function makeCat(name, age) {
   //add an age property to the object with the value set to the age argument
   //add a method called meow that returns the string 'Meow!'
   //return the object
+  var cat = {
+    name: name,
+    age: age,
+    meow: function() {
+      return 'Meow!';
+    }
+  };
+
+  return cat;
 }
 
 function addProperty(object, property) {
   //add the property to the object with a value of null
   //return the object
-  //note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  //note: the property name is NOT 'property'.
+  //The name is the value of the argument called property (a string)
+  object[property] = null;
+  return object;
 }
 
 function invokeMethod(object, method) {
   //method is a string that contains the name of a method on the object
   //invoke this method
   //nothing needs to be returned
+  object[method()];
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
@@ -83,7 +96,7 @@ function addCalculateDiscountPriceMethod(storeItem) {
   //add a method to the storeItem object called 'calculateDiscountPrice'
   //this method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
   //the method then subtracts the discount from the price and returns the discounted price
-  //example: 
+  //example:
   //price -> 20
   //discountPercentage -> .2
   //discountPrice = 20 - (20 * .2)
