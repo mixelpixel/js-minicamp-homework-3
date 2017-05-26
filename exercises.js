e4ac639ba35f88a5eba6bb85e2cc2ee597cf4797
mixelpixel <mixelpix@mac.com>
2017-05-26 03:51:37 -0400

    Homework 3

--- exercises.js
+++ exercises.js
@@ -31,50 +31,73 @@ function invokeMethod(object, method) {
   //method is a string that contains the name of a method on the object
   //invoke this method
   //nothing needs to be returned
-  object[method()];
+  object[method]();
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   //mysteryNumberObject has a property called mysteryNumber
   //multiply the mysteryNumber property by 5 and return the product
+  var product = mysteryNumberObject.mysteryNumber * 5;
+  return product;
 }
 
 function deleteProperty(object, property) {
   //remove the property from the object
   //return the object
+  delete object[property];
+  return object;
 }
 
 function newUser(name, email, password) {
   //create a new object with properties matching the arguments passed in.
   //return the new object
+  var newObject = {
+    name: name,
+    email: email,
+    password: password
+  };
+  return newObject;
 }
 
 function hasEmail(user) {
   //return true if the user has a value for the property 'email'
   //otherwise return false
+  if (user['email']) {
+    return true;
+  } else return false;
 }
 
 function hasProperty(object, property) {
   //return true if the object has the value of the property argument
   //property is a string
   //otherwise return false
+  if (object[property]) {
+    return true;
+  } else return false;
 }
 
 function verifyPassword(user, password) {
   //check to see if the provided password matches the password property on the user object
   //return true if they match
   //otherwise return false
+  if (user.password === password) {
+    return true;
+  } else return false;
 }
 
 function updatePassword(user, newPassword) {
   //replace the existing password on the user object with the value of newPassword
   //return the object
+  user.password = newPassword;
+  return user;
 }
 
 function addFriend(user, newFriend) {
   //user has a property called friends that is an array
   //add newFriend to the end of the friends array
   //return the user object
+  user.friends.push(newFriend);
+  return user;
 }
 
 function setUsersToPremium(users) {
@@ -82,6 +105,9 @@ function setUsersToPremium(users) {
   //each user object has the property 'isPremium'
   //set each user's isPremium property to true
   //return the users array
+  for (var i = 0; i < users.length; i++) {
+    users[i].isPremium = true;}
+  return users;
 }
 
 function sumUserPostLikes(user) {
@@ -90,6 +116,11 @@ function sumUserPostLikes(user) {
   //each post object has an integer property called 'likes'
   //sum together the likes from all the post objects
   //return the sum
+  var sum = 0;
+  for (var i = 0; i < user.posts.length; i++) {
+    sum += user.posts[i].likes;
+  }
+  return sum;
 }
 
 function addCalculateDiscountPriceMethod(storeItem) {
@@ -100,6 +131,12 @@ function addCalculateDiscountPriceMethod(storeItem) {
   //price -> 20
   //discountPercentage -> .2
   //discountPrice = 20 - (20 * .2)
+storeItem.calculateDiscountPrice = function() {
+  var discount = storeItem.price * storeItem.discountPercentage;
+  var discountPrice = storeItem.price - discount;
+  return discountPrice;
+  };
+  return storeItem;
 }
 
 //Do not modify code below this line.
